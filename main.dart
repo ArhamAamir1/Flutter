3e261d92fe0244afa7be9302b995702a5f7b3bbf
ArhamAamir1 <arham.hashmani.101@gmail.com>
2024-07-27 15:51:22 +0500

    assignment5

---
+++ main.dart
@@ -0,0 +1,30 @@
+void main() {
+  // Create a list of names and print all names using list;
+  String name1 = 'yonus';
+  String name2 = 'haseeb';
+  String name3 = 'anas';
+  String name4 = 'ali';
+  List studentname = ['yonus', 'haseeb', 'anas', 'ali'];
+  print(studentname);
+  // Create a list of Days and print only  Sunday;
+  String day1 = 'monday';
+  String day2 = 'tuesday';
+  String day3 = 'wednesday';
+  String day4 = 'thursday';
+  String day5 = 'friday';
+  String day6 = 'saturday';
+  String day7 = 'sunday';
+  List weekdays = [
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday'
+  ];
+  print(weekdays[weekdays.length - 1]);
+  // create a list  of name, class, roll no, grade, percentage and print;
+  List stdinfo = ['ARHAM', '12', '322953', 'A', '74.96'];
+  print(stdinfo);
+}
